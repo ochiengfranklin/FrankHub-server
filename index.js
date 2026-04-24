5567ffee988b6eddfaf5724c078e5b361129a255
Franklin Ochieng <ochiengfranklin264@gmail.com>
2026-04-24 16:50:15 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import path from 'path'
+import connectDB from './config/db.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+dotenv.config({ path: path.join(__dirname, '.env') })
+console.log('MONGO_URI:', process.env.MONGO_URI)
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('FrankHub API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
